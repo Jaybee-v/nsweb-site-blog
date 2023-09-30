56d551db83a511a49df080587ba6d72ff723da85
Jean-Baptiste VRAIN <j-b.vrain@outlook.fr>
2023-09-30 17:32:38 +0200

    admin auth done

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,26 +1,16 @@
+"use client"
 
-'use client'
-
-import {  useSession } from "next-auth/react"
-import { useEffect } from "react"
+import { useSession } from "next-auth/react"
 import { useRouter } from "next/navigation"
 import { Login } from "@/components/Admin/Auth/Login"
 
-export default  function BlogLayout({
+export default function BlogLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
-const router = useRouter()
-    const {data} = useSession()
+    const router = useRouter()
+    const { data } = useSession()
 
-    return (
-<div>
-<Login/>
-
-{children}
-</div>
-  
-            )
-        
+    return <div>{!data ? <Login /> : <>{children}</>}</div>
 }
